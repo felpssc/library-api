d7777f3a45471c6b41e4fbdf6b17cbe3be88e065
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2021-10-30 22:46:27 -0300

    ✨ Add server and author routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,19 @@ import express from 'express';
 
 import './database';
 
+import { router } from './routes/server.routes';
+import { router as authorRouter } from './routes/author.routes';
+
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.redirect('/api');
+});
+
+app.use('/api', router);
+app.use('/api/author', authorRouter);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
